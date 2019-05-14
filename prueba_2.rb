a6459b492a2c08838ebb47249d860d6efcc2ffd0
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-05-14 13:10:53 -0400

    Opción 1 agregada

--- prueba_2.rb
+++ prueba_2.rb
@@ -4,3 +4,20 @@ def read_alum(file_name)
     file.close
     alum
 end
+
+
+def promedio
+    alum = read_alum('alumnos.csv') 
+        alum.each do |arreglo|
+        notas = arreglo[1..5]
+        suma = 0
+        notas.each do |nota|
+            suma += nota.to_i.to_f
+        end
+        
+        division = suma/notas.length
+        puts "El promedio de #{arreglo[0]} es: #{division}"
+        end
+    end
+
+promedio
